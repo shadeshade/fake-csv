39a41de2066a3afaff41708b82be0a9b9df1a1b9
Abdul Magomedov <privetabdul@gmail.com>
2021-08-13 13:39:05 +0300

    Add models Schema, Column

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,8 +125,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/users/login/'
--- schemas/models.py
+++ schemas/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from . import choices
+
+
+class Schema(models.Model):
+    added_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    column_separator = models.CharField(
+        max_length=50, choices=choices.SEPARATORS, default=choices.COM)
+    string_character = models.CharField(
+        max_length=50, choices=choices.QUOTATION_CHARACTERS, default=choices.DBL)
+
+
+class Column(models.Model):
+    column_name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50, choices=choices.TYPES)
+    range_from = models.PositiveIntegerField(blank=True, null=True)
+    range_to = models.PositiveIntegerField(blank=True, null=True)
+    quantity = models.PositiveIntegerField(blank=True, null=True)
+    order = models.PositiveIntegerField(blank=False, null=False)
+    schema = models.ForeignKey(Schema, on_delete=models.CASCADE)
