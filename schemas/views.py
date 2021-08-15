03a7226c2c4b4748f396b5968647cbcc46280077
Abdul Magomedov <privetabdul@gmail.com>
2021-08-16 00:19:28 +0300

    add SchemaDetailView, SchemaUpdateView, SchemaDeleteView.  Refactor views.py

--- schemas/views.py
+++ schemas/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from django.db import transaction, IntegrityError
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, TemplateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 
-from .forms import SchemaCreateForm, ColumnCreateFormSet
-from .models import Schema
+from .forms import SchemaForm, ColumnUpdateFormSet, ColumnCreateFormSet
+from .models import Schema, Column
 
 
 class SchemaListView(ListView):
@@ -13,29 +14,81 @@ class SchemaListView(ListView):
     paginate_by = 25
 
 
-class SchemaCreateView(TemplateView):
+class SchemaCreateView(LoginRequiredMixin, CreateView):
     template_name = 'schemas/schema_create.html'
 
     def get_context_data(self, **kwargs):
-        kwargs['schema_form'] = SchemaCreateForm()
-        kwargs['column_form'] = ColumnCreateFormSet()
+        kwargs['schema_form'] = SchemaForm()
+        kwargs['column_form'] = ColumnCreateFormSet(queryset=Column.objects.none())
         return kwargs
 
     def post(self, request, *args, **kwargs):
-        schema_form = SchemaCreateForm(request.POST)
+        schema_form = SchemaForm(request.POST)
         column_form = ColumnCreateFormSet(request.POST)
 
-        if schema_form.is_valid() and column_form.is_valid():
+        if schema_form.is_valid() and column_form.is_valid():  # Check if submitted forms are valid
             try:
                 with transaction.atomic():
                     schema = schema_form.save(commit=False)
                     schema.added_by = request.user
                     schema.save()
 
-                    for column in column_form:
-                        col = column.save(commit=False)
-                        col.schema = schema
-                        col.save()
+                    for form in column_form:
+                        column = form.save(commit=False)
+                        column.schema = schema
+                        column.save()
             except IntegrityError:
                 print('Error Encountered')
             return redirect(reverse_lazy('schemas:schema_list'))
+
+
+class SchemaDetailView(DetailView):
+    model = Schema
+    template_name = 'schemas/schema_detail.html'
+
+
+class SchemaUpdateView(UserPassesTestMixin, UpdateView):
+    model = Schema
+    template_name = 'schemas/schema_update.html'
+    fields = ['name', 'column_separator', 'string_character', ]
+
+    def get_context_data(self, **kwargs):
+        kwargs['schema_form'] = SchemaForm(instance=self.object)
+        kwargs['column_form'] = ColumnUpdateFormSet(queryset=Column.objects.filter(schema=self.object))
+        return kwargs
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        schema_form = SchemaForm(instance=self.object, data=request.POST)
+        column_form = ColumnUpdateFormSet(request.POST)
+
+        if schema_form.is_valid() and column_form.is_valid():  # Check if submitted forms are valid
+            try:
+                with transaction.atomic():
+                    schema_form.save()
+
+                    Column.objects.filter(schema=self.object).delete()
+
+                    for form in column_form:
+                        column = form.save(commit=False)
+                        column.schema = self.object
+                        column.save()
+            except IntegrityError:
+                print('Error Encountered')
+        return redirect(reverse_lazy('schemas:schema_list'))
+
+    def test_func(self):
+        schema = self.get_object()
+        user = self.request.user
+        return schema.added_by == user or user.is_staff
+
+
+class SchemaDeleteView(UserPassesTestMixin, DeleteView):
+    model = Schema
+    template_name = 'delete_confirm.html'
+    success_url = reverse_lazy('schema_list')
+
+    def test_func(self):
+        schema = self.get_object()
+        user = self.request.user
+        return schema.added_by == user or user.is_staff
