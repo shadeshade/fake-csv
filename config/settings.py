a61f3f9333dcb3da8d460f7e358413ddfce81856
Abdul Magomedov <privetabdul@gmail.com>
2021-08-27 14:44:58 +0300

    Update static root path

--- config/settings.py
+++ config/settings.py
@@ -134,8 +134,9 @@ CELERY_IMPORTS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # MEDIA
 MEDIA_URL = '/media/'
