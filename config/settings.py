44cc4843c09342f546ee2725d90074b490ca4e17
Abdul Magomedov <privetabdul@gmail.com>
2021-08-28 12:03:32 +0300

    Update db settings

--- config/settings.py
+++ config/settings.py
@@ -89,6 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 #         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
